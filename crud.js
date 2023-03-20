a846a10f15e1923b19713c6006aca9031ea4e964
unknown <nadilakencana1107@gmail.com>
2023-03-20 11:07:55 +0700

    crud javascript

---
+++ crud.js
@@ -0,0 +1,33 @@
+let karyawan =[];
+
+function showKaryawan (){
+    let listKaryawan = document.getElementById('listkaryawan');
+    //console.log(listKaryawan);
+    listKaryawan.innerHTML = "";
+
+    for(let i= 0; i< karyawan.length; i++){
+        var btnEdit = "<a href='#' onclick='editKaryawan("+i+")'>Edit</a>";
+        var btnHapus = "<a href='#' onclick='hapusData("+i+")'>hapus</a>";
+
+        listKaryawan.innerHTML += "<li>"+ karyawan[i] +" ["+btnEdit+"]" + " ["+btnHapus+"]" +"</li>";
+
+    }
+}
+
+function addKaryawan(){
+    let input = document.getElementById('nama');
+    karyawan.push(input.value);
+    showKaryawan();
+}
+
+function editKaryawan(id){
+    let newNama = prompt('inputkan nama baru', karyawan[id]);
+    karyawan[id] = newNama;
+    showKaryawan();
+
+}
+
+function hapusData(id){
+    karyawan.splice(id, 1);
+    showKaryawan();
+}
